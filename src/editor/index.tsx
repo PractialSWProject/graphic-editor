3e1bf77227d319a9b2b5ef65a64169e114ff31f0
hypnotes <hyp0notes@gmail.com>
2023-10-22 06:44:02 +0900

    rfc: seprate toolbar from editor

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,21 +1,13 @@
-import { useEffect, useState, useMemo } from 'react'
-import { FilledShapeConcreteFactory, OutlinedShapeConcreteFactory } from '../models/shapes/variants'
-import { TextConcreteFactory } from '../models/text'
+import { useState, useMemo } from 'react'
 import { Stage, Layer } from 'react-konva'
-import { Button } from '@mui/material'
+import { Box } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
-import { ShapeElementT, TextElementT, VariantT } from './type'
-import { DEFAULT_POS, DEFAULT_SIZE, DEFAULT_COLOR, DEFAULT_CONTENT, DEFAULT_FONTSIZE } from './constants'
+import { ShapeElementT, TextElementT } from './type'
 import RectView from './ElementView/RectView'
 import EllipseView from './ElementView/EllipseView'
 import TextView from './ElementView/TextView'
-
-let ZINDEX = 0
-let ID = 1
-
-const filledShapeFactory = new FilledShapeConcreteFactory()
-const outlinedShapeFactory = new OutlinedShapeConcreteFactory()
-const textFactory = new TextConcreteFactory()
+import ToolBar from './ToolBar'
+import PropertyWindow from './PropertyWindow'
 
 function Editor() {
   const [rects, setRects] = useState<ShapeElementT[]>([])
@@ -73,124 +65,60 @@ function Editor() {
     }
   ]
 
-  const createRectangle = (variant: VariantT) => {
-    const newRect = {
-      id: ID++,
-      variant,
-      shape:
-        variant === 'filled'
-          ? filledShapeFactory.createRectangle({
-              position: { ...DEFAULT_POS },
-              size: { ...DEFAULT_SIZE },
-              color: DEFAULT_COLOR,
-              zIndex: ZINDEX++
-            })
-          : outlinedShapeFactory.createRectangle({
-              position: { ...DEFAULT_POS },
-              size: { ...DEFAULT_SIZE },
-              color: DEFAULT_COLOR,
-              zIndex: ZINDEX++
-            })
-    }
-
+  const handleUpdateRect = (newRect: ShapeElementT) => {
     setRects([...rects, newRect])
   }
 
-  const createEllipse = (variant: VariantT) => {
-    const newEllipse = {
-      id: ID++,
-      variant,
-      shape:
-        variant === 'filled'
-          ? filledShapeFactory.createEllipse({
-              position: { ...DEFAULT_POS },
-              size: { ...DEFAULT_SIZE },
-              color: DEFAULT_COLOR,
-              zIndex: ZINDEX++
-            })
-          : outlinedShapeFactory.createEllipse({
-              position: { ...DEFAULT_POS },
-              size: { ...DEFAULT_SIZE },
-              color: DEFAULT_COLOR,
-              zIndex: ZINDEX++
-            })
-    }
-
+  const handleUpdateEllipse = (newEllipse: ShapeElementT) => {
     setEllipses([...ellipses, newEllipse])
   }
 
-  const createText = () => {
-    const newText = {
-      id: ID++,
-      text: textFactory.createText({
-        position: { ...DEFAULT_POS },
-        size: { ...DEFAULT_SIZE },
-        content: DEFAULT_CONTENT,
-        textColor: DEFAULT_COLOR,
-        fontSize: DEFAULT_FONTSIZE,
-        zIndex: ZINDEX++
-      })
-    }
-
+  const handleUpdateText = (newText: TextElementT) => {
     setTexts([...texts, newText])
   }
 
-  useEffect(() => {
-    let recElement1 = {
-      id: ID++,
-      variant: 'filled' as VariantT,
-      shape: filledShapeFactory.createRectangle({
-        position: { x: 50, y: 50 },
-        size: { width: 100, height: 100 },
-        color: 'blue',
-        zIndex: ZINDEX++
-      })
-    }
-    let recElement2 = {
-      id: ID++,
-      variant: 'filled' as VariantT,
-      shape: filledShapeFactory.createRectangle({
-        position: { x: 200, y: 200 },
-        size: { width: 100, height: 100 },
-        color: 'red',
-        zIndex: ZINDEX++
-      })
-    }
-    setRects([recElement1, recElement2])
-  }, [])
-
   return (
-    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-      <Stage
-        width={window.innerWidth * 0.7}
-        height={window.innerHeight}
-        style={{ display: 'inline-block', border: '1px solid gray' }}
-      >
-        <Layer>
-          <RectView rects={rects} setRects={setRects} />
-          <EllipseView ellipses={ellipses} setEllipses={setEllipses} />
-          <TextView texts={texts} setTexts={setTexts} />
-        </Layer>
-      </Stage>
-      <div>
-        <DataGrid rows={rows} columns={columns} pagination />
-        <Button style={{ display: 'block' }} onClick={() => createRectangle('filled')}>
-          Create new filled Rectangle
-        </Button>
-        <Button style={{ display: 'block' }} onClick={() => createRectangle('outlined')}>
-          Create new outlined Rectangle
-        </Button>
-        <Button style={{ display: 'block' }} onClick={() => createEllipse('filled')}>
-          Create new filled Ellipse
-        </Button>
-        <Button style={{ display: 'block' }} onClick={() => createEllipse('outlined')}>
-          Create new outlined Ellipse
-        </Button>
-        <Button style={{ display: 'block' }} onClick={() => createText()}>
-          Create new Text
-        </Button>
-      </div>
-    </div>
+    <Box sx={{ width: '100vw', height: '100vh', display: 'flex' }}>
+      <Box sx={{ width: '70vw', height: '100vh' }}>
+        <ToolBar
+          rects={rects}
+          ellipses={ellipses}
+          texts={texts}
+          setRects={handleUpdateRect}
+          setEllipses={handleUpdateEllipse}
+          setTexts={handleUpdateText}
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            height: '90vh',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'darkgray'
+          }}
+        >
+          <Stage
+            width={500}
+            height={500}
+            style={{ display: 'inline-block', border: '1px solid gray', background: 'white' }}
+          >
+            <Layer>
+              <RectView rects={rects} setRects={setRects} />
+              <EllipseView ellipses={ellipses} setEllipses={setEllipses} />
+              <TextView texts={texts} setTexts={setTexts} />
+            </Layer>
+          </Stage>
+        </Box>
+      </Box>
+      <Box sx={{ width: '30vw', height: '100vh' }}>
+        <Box sx={{ height: '50vh' }}>
+          <PropertyWindow />
+        </Box>
+        <Box sx={{ height: '50vh' }}>
+          <DataGrid rows={rows} columns={columns} pagination />
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
