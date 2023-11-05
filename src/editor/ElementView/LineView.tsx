7daf6b7dc8922e8064a581d7444c6ee617ace402
hypnotes <hyp0notes@gmail.com>
2023-11-06 01:01:02 +0900

    mod: remove listenders and treat View as a single shape

--- src/editor/ElementView/LineView.tsx
+++ src/editor/ElementView/LineView.tsx
@@ -1,52 +1,39 @@
-import { useState } from 'react'
 import { Group } from 'react-konva'
-import CreatedComposite from '../../models/composite/created'
 import { KonvaEventObject } from 'konva/lib/Node'
 import { Line } from 'react-konva'
+import Elements from '../../models/Elements'
 
 interface Props {
-  createdComposite: CreatedComposite
+  el: Elements
   handleMove: (e: KonvaEventObject<DragEvent>, isLine?: boolean) => void
   handleEnlarge: (e: KonvaEventObject<Event>, isLine?: boolean) => void
 }
 
-const LineView = ({ createdComposite, handleMove, handleEnlarge }: Props) => {
-  const [updateLineFlag, setUpdateLineFlag] = useState(false)
-
-  createdComposite.listenForLineChanges(() => {
-    setUpdateLineFlag(!updateLineFlag)
-  })
-
-  const lines = createdComposite.getLines()
-  console.log('lines', lines)
-
+const LineView = ({ el, handleMove, handleEnlarge }: Props) => {
+  if (el.deleted) return null
   return (
-    <>
-      {lines.map(el => (
-        <Group key={el.id}>
-          <Line
-            id={el.id.toString()}
-            x={0}
-            y={0}
-            points={[
-              el.properties.position.x,
-              el.properties.position.y,
-              el.properties.position.x + el.properties.size.width,
-              el.properties.position.y + el.properties.size.height
-            ]}
-            shadowBlur={10}
-            shadowColor="lime"
-            shadowEnabled={el.selected ? true : false}
-            stroke={el.properties.color}
-            strokeWidth={5}
-            onDragEnd={e => handleMove(e, true)}
-            onTransformEnd={e => handleEnlarge(e, true)}
-            zIndex={el.properties.zIndex}
-            draggable
-          />
-        </Group>
-      ))}
-    </>
+    <Group key={el.id}>
+      <Line
+        id={el.id.toString()}
+        x={0}
+        y={0}
+        points={[
+          el.properties.position.x,
+          el.properties.position.y,
+          el.properties.position.x + el.properties.size.width,
+          el.properties.position.y + el.properties.size.height
+        ]}
+        shadowBlur={10}
+        shadowColor="lime"
+        shadowEnabled={el.selected ? true : false}
+        stroke={el.properties.color}
+        strokeWidth={5}
+        onDragEnd={e => handleMove(e, true)}
+        onTransformEnd={e => handleEnlarge(e, true)}
+        zIndex={el.properties.zIndex}
+        draggable
+      />
+    </Group>
   )
 }
 
--- src/editor/ElementView/RectView.tsx
+++ src/editor/ElementView/RectView.tsx
@@ -1,46 +1,35 @@
-import { useState } from 'react'
 import { Group } from 'react-konva'
 import { KonvaEventObject } from 'konva/lib/Node'
-import CreatedComposite from '../../models/composite/created'
 import { Rect } from 'react-konva'
+import Elements from '../../models/Elements'
 
 interface Props {
-  createdComposite: CreatedComposite
+  el: Elements
   handleMove: (e: KonvaEventObject<DragEvent>) => void
   handleEnlarge: (e: KonvaEventObject<Event>) => void
 }
 
-const RectView = ({ createdComposite, handleMove, handleEnlarge }: Props) => {
-  const [updateRectFlag, setUpdateRectFlag] = useState(false)
-
-  createdComposite.listenForRectChanges(() => {
-    setUpdateRectFlag(!updateRectFlag)
-  })
-
-  const rectangles = createdComposite.getRectangles()
+const RectView = ({ el, handleMove, handleEnlarge }: Props) => {
+  if (el.deleted) return null
 
   return (
-    <>
-      {rectangles.map(el => (
-        <Group key={el.id}>
-          <Rect
-            id={el.id.toString()}
-            x={el.properties.position.x}
-            y={el.properties.position.y}
-            width={el.properties.size.width}
-            height={el.properties.size.height}
-            fill={el.properties.color}
-            shadowBlur={10}
-            shadowColor="lime"
-            shadowEnabled={el.selected ? true : false}
-            onDragEnd={e => handleMove(e)}
-            onTransformEnd={e => handleEnlarge(e)}
-            zIndex={el.properties.zIndex}
-            draggable
-          />
-        </Group>
-      ))}
-    </>
+    <Group key={el.id}>
+      <Rect
+        id={el.id.toString()}
+        x={el.properties.position.x}
+        y={el.properties.position.y}
+        width={el.properties.size.width}
+        height={el.properties.size.height}
+        fill={el.properties.color}
+        shadowBlur={10}
+        shadowColor="lime"
+        shadowEnabled={el.selected ? true : false}
+        onDragEnd={e => handleMove(e)}
+        onTransformEnd={e => handleEnlarge(e)}
+        zIndex={el.properties.zIndex}
+        draggable
+      />
+    </Group>
   )
 }
 
