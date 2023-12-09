075f10098974806851388232a6ddf3c7162695c3
hypnotes <hyp0notes@gmail.com>
2023-12-10 06:30:43 +0900

    mod: renmaing files

--- src/models/elementAbstract.ts
+++ src/models/elementAbstract.ts
@@ -1,4 +1,4 @@
-// base.ts
+// just interfaces for the concrete elements
 export interface Position {
   x: number
   y: number
@@ -8,6 +8,15 @@ export interface Size {
   width: number
   height: number
 }
+
+export type ZIndex = number
+export type Color = string
+
+export const DEFAULT_POS = { x: 100, y: 100 }
+export const DEFAULT_SIZE = { width: 100, height: 100 }
+export const DEFAULT_COLOR = '#4a4a4a'
+export const DEFAULT_CONTENT = 'This is text'
+export const DEFAULT_FONTSIZE = 15
 
 export const ELLIPSE = 'ELLIPSE'
 export const RECTANGLE = 'RECTANGLE'
@@ -41,4 +50,28 @@ export interface AbstractText extends AbstractElement {
   getColor(): string
   getFontSize(): number
   getContent(): string
+}
+// Abstract Factory
+export interface AbstractFactory {
+  createImage(
+    id: number,
+    url: string,
+    size: Size,
+    position: Position,
+    zIndex: number,
+    isSelected?: boolean
+  ): AbstractImage
+
+  createShape(id: number, shapeType: string): AbstractShape
+
+  createText(
+    id: number,
+    content: string,
+    position: Position,
+    size: Size,
+    zIndex: number,
+    color: string,
+    fontSize: number,
+    isSelected?: boolean
+  ): AbstractText
 }
--- src/models/elementConcrete.ts
+++ src/models/elementConcrete.ts
@@ -1,8 +1,12 @@
-// concrete.ts
-import getNewPosition from '../utils/newPosition'
-import getRandomColor from '../utils/randomColor'
-import { DEFAULT_COLOR, DEFAULT_FONTSIZE, DEFAULT_POS, DEFAULT_SIZE } from './base'
-import { AbstractElement, AbstractImage, AbstractShape, AbstractText, Position, ShapeType, Size } from './abstract'
+import {
+  AbstractElement,
+  AbstractImage,
+  AbstractShape,
+  AbstractText,
+  Position,
+  ShapeType,
+  Size
+} from './elementAbstract'
 
 export abstract class ConcreteElement implements AbstractElement {
   protected id: number
@@ -149,68 +153,5 @@ export class ConcreteText extends ConcreteElement implements AbstractText {
 
   setColor(color: string): void {
     this.color = color
-  }
-}
-
-// Abstract Factory
-export interface AbstractFactory {
-  createImage(
-    id: number,
-    url: string,
-    size: Size,
-    position: Position,
-    zIndex: number,
-    isSelected?: boolean
-  ): AbstractImage
-
-  createShape(id: number, shapeType: string): AbstractShape
-
-  createText(
-    id: number,
-    content: string,
-    position: Position,
-    size: Size,
-    zIndex: number,
-    color: string,
-    fontSize: number,
-    isSelected?: boolean
-  ): AbstractText
-}
-
-export class ConcreteFactory implements AbstractFactory {
-  createImage(
-    id: number,
-    url: string,
-    size: Size,
-    position: Position = DEFAULT_POS,
-    zIndex: number = id,
-    isSelected: boolean = false
-  ): ConcreteImage {
-    return new ConcreteImage(id, url, size, position, zIndex, isSelected)
-  }
-
-  createShape(
-    id: number,
-    shapeType: ShapeType,
-    position: Position = getNewPosition(),
-    size: Size = DEFAULT_SIZE,
-    zIndex: number = id,
-    color: string = getRandomColor(),
-    isSelected: boolean = false
-  ): ConcreteShape {
-    return new ConcreteShape(id, shapeType, position, size, zIndex, color, isSelected)
-  }
-
-  createText(
-    id: number,
-    content: string,
-    position: Position = DEFAULT_POS,
-    size: Size = DEFAULT_SIZE,
-    zIndex: number = id,
-    color: string = DEFAULT_COLOR,
-    fontSize: number = DEFAULT_FONTSIZE,
-    isSelected: boolean = false
-  ): ConcreteText {
-    return new ConcreteText(id, content, position, size, zIndex, color, fontSize, isSelected)
   }
 }
