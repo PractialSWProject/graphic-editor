ae384f4cce69f1987c530aecf530ff5439c94c29
hypnotes <hyp0notes@gmail.com>
2023-12-10 06:19:47 +0900

    rmv: usunsed files

--- src/models/Elements/Shapes/line.ts
+++
@@ -1,9 +0,0 @@
-import { Shapes } from '..'
-
-class Line extends Shapes {
-  setSelect(selected: boolean): void {
-    this.selected = selected
-  }
-}
-
-export default Line
--- src/models/Elements/Shapes/rectangle.ts
+++
@@ -1,9 +0,0 @@
-import { Shapes } from '..'
-
-class Rectangle extends Shapes {
-  setSelect(selected: boolean): void {
-    this.selected = selected
-  }
-}
-
-export default Rectangle
